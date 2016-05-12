c12f67096765a4c658ff070f07d4d35a14eac2b5
FezZHD <zhenya535@gmail.com>
2016-05-12 10:23:32 +0300

    adding some functions

--- pop3wpf/pop3wpf/Messages.cs
+++ pop3wpf/pop3wpf/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,27 +9,43 @@ namespace pop3wpf
 {
     class Messages
     {
-        byte[] bufferBytes = new byte[8127];
+        private byte[] _bufferBytes = new byte[8127];
 
 
         public int GetMessageCount()
         {
+            int result;
             string data;
             MailAccept.MailSslStream.Write(Encoding.ASCII.GetBytes("STAT\r\n"));
-            int bytesRead = MailAccept.MailSslStream.Read(bufferBytes, 0, bufferBytes.Length);
-            data = Encoding.UTF8.GetString(bufferBytes, 0, bytesRead);
+            int bytesRead = MailAccept.MailSslStream.Read(_bufferBytes, 0, _bufferBytes.Length);
+            data = Encoding.UTF8.GetString(_bufferBytes, 0, bytesRead);
             if (data.Contains("+OK"))
             {
                 data = data.Remove(0, 4);
                 string[] parsedArgiments = data.Split(' ');
-                int result; 
                 Int32.TryParse(parsedArgiments[0], out result);
-                return result;
             }
             else
             {
-                return -1;
+                result = -1;
+            } 
+            return result;
+        }
+
+        public string[] GetMessageList()
+        {
+            string result;
+            MailAccept.MailSslStream.Write(Encoding.ASCII.GetBytes("LIST\r\n"));
+            int bytesRead = MailAccept.MailSslStream.Read(_bufferBytes, 0, _bufferBytes.Length);
+            result = Encoding.ASCII.GetString(_bufferBytes, 0, bytesRead);
+            if ((bytesRead != 0) && (!result.Contains("\r\n.\r\n")))
+            {
+                bytesRead = MailAccept.MailSslStream.Read(_bufferBytes, 0, _bufferBytes.Length);
+                result += Encoding.ASCII.GetString(_bufferBytes, 0, bytesRead);
             }
+            result = result.Replace('\r',' ');
+            string[] resultArray = result.Split('\n');
+            return resultArray;
         }
     }
 }
